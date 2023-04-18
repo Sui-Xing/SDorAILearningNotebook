0c6b7d0d3e989f91b3d21fc5e299ba17cf4b4229
Jupiter <1002911359@qq.com>
2023-04-18 17:36:34 +0800

    笔记更新

---
+++ 算法/god_zuo/Tree.java
@@ -0,0 +1,8 @@
+public class Tree {
+    Tree left=null;
+    Tree right=null;
+    int val=0;
+    public Tree(int val){
+        this.val=val;
+    }
+}
---
+++ 算法/god_zuo/searchTree.java
@@ -0,0 +1,35 @@
+public class searchTree {
+    public static void main(String[] args) {
+
+        Tree head=new Tree(6);
+        Tree t1=new Tree(5);
+        Tree t2=new Tree(4);
+        Tree t3=new Tree(3);
+        Tree t4=new Tree(7);
+        Tree t5=new Tree(9);
+        head.left=t1;
+        head.right=t4;
+        t1.left=t5;
+
+        System.out.println(isBst(head));
+
+
+    }
+    public static int preValue=Integer.MIN_VALUE;
+    public static boolean isBst(Tree head){
+        if(head==null){
+            return true;
+        }
+        boolean isLeftBst=isBst(head.left);
+        if(!isLeftBst){
+            return false;
+        }
+        if(head.val<=preValue){
+            return false;
+        }else{
+            preValue=head.val;
+        }
+        return isBst(head.right);
+    }
+
+}
