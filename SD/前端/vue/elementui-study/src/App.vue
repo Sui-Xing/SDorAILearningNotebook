1cd8d16ad17798aae3abc0e27c3e60656aa4dba6
Sui-Xing <1002911359@qq.com>
2024-06-24 10:41:04 +0800

    update dir

---
+++ SD/前端/vue/elementui-study/src/App.vue
@@ -0,0 +1,43 @@
+<template >
+  <div id="app">
+    <img src="src/assets/logo.png">
+
+          <Main>
+            <HelloWorld slot="HelloWorld" :testdata="testdata"></HelloWorld>
+            <Login slot="Login"></Login>
+          </Main>
+
+<!--    <test></test>-->
+    <br>
+
+
+<!--    <router-view></router-view>-->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+import test from './views/test'
+import Main from './views/main'
+import Login from './views/Login'
+
+
+export default {
+  name: 'App',
+  props:['testdata'],
+  components: {
+    HelloWorld,test,Main,Login
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
